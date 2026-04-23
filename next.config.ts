117b3634464719c4371fbb5cc03fd9e5e8315a10
Bikal Niraula <134396940+bikalll@users.noreply.github.com>
2026-04-24 01:19:00 +0545

    Clean

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
